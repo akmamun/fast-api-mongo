6aa862189b3c122e076ebcd5c87a302ac2acc367
akmamun <almamunkhan04@gmail.com>
2021-02-13 23:55:25 +0600

    added basic arch

--- src/server.py
+++ src/server.py
@@ -10,5 +10,10 @@ async def index():
     return {"Hello": "World"}
 
 
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+
 if __name__ == "main":
     app.run()
