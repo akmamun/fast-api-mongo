6688dc43b94487dcb6032d43b4921e4ba2d9b33a
Al Mamun Khan <almamunkhan04@gmail.com>
2021-07-12 19:30:53 +0600

    Update routes.py

--- src/servers/routes.py
+++ src/servers/routes.py
@@ -6,8 +6,8 @@ from src.servers.controllers.todo_controller import TodoController
 def index():
     return {"message": "base route of service"}
 
-
+TodoController = TodoController()
 routes = [
     APIRoute('/', index, methods=["GET"]),
-    APIRoute('/todo', TodoController().get_todos, methods=["GET"])
+    APIRoute('/todo', TodoController.get_todos, methods=["GET"])
 ]
