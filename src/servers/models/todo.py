cb97c230ac7cc608881f542f820eb28883aa48c5
akmamun <almamunkhan04@gmail.com>
2021-07-14 00:01:52 +0600

    get todos done

--- src/servers/models/todo.py
+++ src/servers/models/todo.py
@@ -11,8 +11,20 @@ class Todos(Repository):
     def collection(self):
         return 'todos'
 
-    def saveTodo(self, title: str, description: str):
+    async def saveTodo(self, title: str, description: str):
         data = {"title": title,
                 "description": description}
 
-        return self.save(data=data, collection_name=self.collection())
+        return await self.save(data=data, collection_name=self.collection())
+
+    async def getAll(self, title: str, start: int = 0, limit: int = 0):
+        qurey = {}
+        if title:
+            qurey['title'] = {'$regex': f'.*{title}.*'}
+        result = await self.find({
+            '$query': qurey,  # filter deleted face
+            '$orderby': {
+                'updated_at': -1
+            }}, self.collection())
+        # result.skip(start).limit(limit)
+        return result
--- src/servers/routes.py
+++ src/servers/routes.py
@@ -6,8 +6,11 @@ from src.servers.controllers.todo_controller import TodoController
 def index():
     return {"message": "base route of service"}
 
+
 TodoController = TodoController()
 routes = [
     APIRoute('/', index, methods=["GET"]),
-    APIRoute('/todos', TodoController.save_todo, methods=["POST"])
+    APIRoute('/todos', TodoController.save_todo, methods=["POST"]),
+    APIRoute('/todos', TodoController.get_todos, methods=["GET"])
+
 ]
